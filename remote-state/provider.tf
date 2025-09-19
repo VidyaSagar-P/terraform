77672c9308571c690e874df17e8ea0eb342a42e8
vidyasagar07 <vidya07422@gmail.com>
2025-09-19 13:49:33 +0530

    terraform practice

---
+++ remote-state/provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.13.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "terraform-remote-state-vidya"
+    key = "remote-state-demo"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locking-file"
+  }
+  
+
+}
+
+
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
